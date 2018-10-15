33618717711974f4318575e70cb52c79b5aff2d8
Arnold Su <arnoldtingsu@gmail.com>
2018-10-15 21:13:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,6 @@ def take_a_number(queue,customer)
 end
 
 def line(queue)
-  if queue = []?:
-    return "someone is line": "no one is in line"
+  if queue = []?: "someone is line": "no one is in line"
   end
 end
