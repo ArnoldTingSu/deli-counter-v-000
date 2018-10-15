bc6453e9a67bc10af08b40aa4d0879928db44b82
Arnold Su <arnoldtingsu@gmail.com>
2018-10-15 21:59:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(queue)
   if queue[0] == nil
     puts "The line is currently empty." 
   else
-    line = "The line is currently:" 
+    line_status = "The line is currently:" 
     
   end
 end
